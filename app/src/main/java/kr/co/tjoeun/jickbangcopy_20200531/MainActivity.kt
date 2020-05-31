2c8baad7258541d4b8463893c38313113b925ee3
cho881020 <kj_cho@idealidea.co.kr>
2020-05-31 12:24:22 +0900

    RoomAdapter뼈대작업 종료

--- app/src/main/java/kr/co/tjoeun/jickbangcopy_20200531/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200531/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.jickbangcopy_20200531
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
