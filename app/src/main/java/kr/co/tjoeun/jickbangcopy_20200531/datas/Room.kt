ee8f38e87b4ce58da160c04519b252658be56bbd
cho881020 <kj_cho@idealidea.co.kr>
2020-05-31 14:04:27 +0900

    1000넘는 숫자는 컴마 붙이도록 처리

--- app/src/main/java/kr/co/tjoeun/jickbangcopy_20200531/datas/Room.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200531/datas/Room.kt
@@ -1,17 +1,20 @@
 package kr.co.tjoeun.jickbangcopy_20200531.datas
 
+import java.text.NumberFormat
+import java.util.*
+
 class Room(val price:Int, val address:String, val floor:Int, val description:String) {
 
     fun getFormattedPrice() : String {
         if (this.price < 10000) {
-            return "${this.price}"
+            return NumberFormat.getInstance(Locale.KOREA).format(this.price)
         }
         else {
 //            정수 / 정수 = 정수 => 28500 / 10000 = 2
 //            28500 % 10000 => 만으로 나눈 나머지 => 8500
             val uk = this.price / 10000
             val underUk = this.price % 10000
-            return "${uk}억 ${underUk}"
+            return "${uk}억 ${NumberFormat.getInstance(Locale.KOREA).format(underUk)}"
         }
     }
 
