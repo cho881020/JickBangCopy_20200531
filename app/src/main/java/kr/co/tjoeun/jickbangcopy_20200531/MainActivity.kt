b6024794356affa84811e7fdcad1c45a45b7ae05
cho881020 <kj_cho@idealidea.co.kr>
2020-05-31 12:39:16 +0900

    방 데이터 ArrayList에 추가

--- app/src/main/java/kr/co/tjoeun/jickbangcopy_20200531/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200531/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.jickbangcopy_20200531.datas.Room
 
 class MainActivity : BaseActivity() {
+
+    val rooms = ArrayList<Room>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,18 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        rooms.add(Room(8000, "마포구 서교동", 1, "마포 서교동의 방입니다."))
+        rooms.add(Room(28000, "마포구 서교동", 3, "마포 서교동의 방입니다."))
+        rooms.add(Room(35000, "마포구 서교동", 5, "마포 서교동의 방입니다."))
+        rooms.add(Room(17000, "마포구 서교동", 4, "마포 서교동의 방입니다."))
+        rooms.add(Room(6000, "마포구 성산동", 0, "마포 성산동의 방입니다."))
+        rooms.add(Room(4000, "마포구 성산동", 1, "마포 성산동의 방입니다."))
+        rooms.add(Room(45000, "마포구 성산동", 0, "마포 성산동의 방입니다."))
+        rooms.add(Room(18000, "마포구 망원동", -1, "마포 망원동의 방입니다."))
+        rooms.add(Room(23000, "마포구 망원동", -1, "마포 망원동의 방입니다."))
+        rooms.add(Room(15000, "마포구 망원동", 2, "마포 망원동의 방입니다."))
+        rooms.add(Room(7000, "마포구 망원동", 3, "마포 망원동의 방입니다."))
 
     }
 
